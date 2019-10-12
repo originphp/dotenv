1a4b3539dea8ded532d06b0ff48ebd5557447da5
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-10-12 16:48:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# DotEnv
+
+![license](https://img.shields.io/badge/license-MIT-brightGreen.svg)
+[![build](https://travis-ci.org/originphp/dotenv.svg?branch=master)](https://travis-ci.org/originphp/dotEnv)
+[![coverage](https://coveralls.io/repos/github/originphp/dotenv/badge.svg?branch=master)](https://coveralls.io/github/originphp/dotEnv?branch=master)
+
+A dotenv parser.
+
+## Installation
+
+To install this package
+
+```linux
+$ composer require originphp/dotenv
+```
+
+
+Then in your application bootstrap
+
+```php
+use Origin\DotEnv\DotEnv;
+(new DotEnv())->load(CONFIG);
+```
+
+Here is an example of a dotenv file.
+
+```linux
+GMAIL_USERNAME=foo@gmail.com
+GMAIL_PASSWORD=secret
+```
+
+You can also add `export` in front of each line so you can source the file with bash.
+
+```linux
+export GMAIL_USERNAME=foo@gmail.com
+export GMAIL_PASSWORD=secret
+```
+
+If you want to use a custom name
+
+```php
+use Origin\DotEnv\DotEnv;
+(new DotEnv())->load(CONFIG,'.env-local');
+```
---
+++ tests/DotEnvTest.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * OriginPHP Framework
+ * Copyright 2018 - 2019 Jamiel Sharief.
+ *
+ * Licensed under The MIT License
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * @copyright   Copyright (c) Jamiel Sharief
+ * @link        https://www.originphp.com
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Origin\Test\DotEnv;
+
+use Origin\DotEnv\DotEnv;
+use \Exception;
+use \InvalidArgumentException;
+
+class MockDotEnv extends DotEnv
+{
+    protected $env = [];
+    protected function env(string $key, $value) : void
+    {
+        $this->env[$key] = $value;
+    }
+    public function getEnv()
+    {
+        return $this->env;
+    }
+}
+
+class DotEnvTest extends \PHPUnit\Framework\TestCase
+{
+    public function testLoadAndParsing()
+    {
+        $dotenv = new MockDotEnv();
+        $dotenv->load(__DIR__, '.env.test'); // TestApp
+        
+        $results = $dotenv->getEnv();
+  
+        $this->assertEquals('3a4e41e63f65b228e2927d6045c09577', md5(json_encode($results)));
+    }
+
+    public function testLoadExecption()
+    {
+        $dotenv = new MockDotEnv();
+        $this->expectException(InvalidArgumentException::class);
+        $dotenv->load(__DIR__);
+    }
+
+    public function testLoadReal()
+    {
+        $dotenv = new DotEnv();
+
+        $directory = sys_get_temp_dir();
+        $file = uniqid();
+
+        $value = bin2hex(random_bytes('32'));
+        file_put_contents($directory . '/' . $file, "ENVTEST_UUID={$value}\n");
+
+        $dotenv->load($directory, $file); // TestApp
+        $this->assertEquals($value, $_ENV['ENVTEST_UUID']);
+    }
+
+    public function testMultiLine()
+    {
+        $key = <<< EOF
+ENVTEST_KEY="-----BEGIN RSA PRIVATE KEY-----
+...
+AbDE7...
+...
+-----END RSA PRIVATE KEY-----"
+EOF;
+        $dotenv = new DotEnv();
+        $directory = sys_get_temp_dir();
+        $file = uniqid();
+        
+        file_put_contents($directory . '/' . $file, $key);
+        $dotenv->load($directory, $file);
+        $this->assertEquals('05a378a68f104bb1a076fed1c5d770ea', md5($_ENV['ENVTEST_KEY']));
+    }
+
+    public function testMultiLineException()
+    {
+        $key = <<< EOF
+ENVTEST_KEY="This is
+a multi line that does not
+end with a quotation
+EOF;
+        $dotenv = new DotEnv();
+        $directory = sys_get_temp_dir();
+        $file = uniqid();
+        
+        file_put_contents($directory . '/' . $file, $key);
+      
+        $this->expectException(Exception::class);
+        $dotenv->load($directory, $file);
+    }
+}
